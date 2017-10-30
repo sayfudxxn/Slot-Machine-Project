b8d5963fadb2bd4cb17b9a3b89da724dabeb99d9
Sayfudxxn <sayfudxxn@gmail.com>
2017-10-30 21:08:24 +0200

    MVC

--- Slot Machine Project/src/slotMachineProject/Main.java
+++ Slot Machine Project/src/slotMachineProject/Main.java
@@ -1,10 +1,22 @@
 package slotMachineProject;
 
-public class Main
-{
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application
+{
 	public static void main(String[] args)
 	{
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("SlotMachineView.fxml"))));
+		primaryStage.setTitle("Slot Machine");
+		primaryStage.show();
 	}
 }
---
+++ Slot Machine Project/src/slotMachineProject/SlotMachineModell.java
@@ -0,0 +1,6 @@
+package slotMachineProject;
+
+public class SlotMachineModell
+{
+
+}
